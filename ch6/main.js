c677ce7202208259551056fd6180053aaa16c543
Andrey Harkusha <xfeaton@gmail.com>
2015-11-03 13:21:33 +0200

    add tasks for chapter 6

--- ch6/main.js
+++ ch6/main.js
@@ -1,4 +1,26 @@
 function wrapValue(n) {
-var localVariable = n;
-return function() { return localVariable + 5; };
+	var localVariable = n;
+	return function() { 
+		return localVariable + 5; 
+		};
+}
+
+function min(a, b){
+	if (a <= b){
+		return a;
+	} else {
+		return b;
+	}
+	
+}
+
+function countBs(Str, Chr){
+	var i = 0, count = 0;
+	while (i<=((Str.length)-1)){
+		if(Chr==Str.charAt(i)){
+			count++;
+		}
+		i++;
+	}
+	return count;
 }
